5d09da472e60edd2f9b7ac3022376a9326328943
Chaitanya Pawar <144248006+ChaitanyaPawar1112@users.noreply.github.com>
2025-08-12 23:06:01 +0530

    Create string_size.cpp

---
+++ string_size.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+char str[]="chaiatnaya pawar";
+int len=0;
+for(int i=0;str[i]!='\0';i++)
+{
+   len++;
+}
+cout<<"size of string:"<<len;
+return 0;
+}
